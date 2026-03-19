07b3a387ab1bdbc059e07a2f8bfedb750c522eba
Diones <pereiradiones987@gmail.com>
2026-03-19 17:22:53 -0300

    Resumo das movimentações

--- b7estoque-backend/src/controllers/dashboard.controller.ts
+++ b7estoque-backend/src/controllers/dashboard.controller.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from 'express';
 import * as dashboardService from '../services/dashboard.service';
+import { dateRangeSchema } from '../validators/dashboard.validator';
 
 export const getInventoryValue = async (req: Request, res: Response) => {
   const totalValue = await dashboardService.getInventoryValue();
   return res.status(200).json({ error: null, data: { totalValue } });
+}
+
+export const getMovesSummary = async (req: Request, res: Response) => {
+  const query = dateRangeSchema.parse(req.query);
+  const data = await dashboardService.getMovesSummary(query);
+  return res.status(200).json({ error: null, data });
 }
--- b7estoque-backend/src/services/dashboard.service.ts
+++ b7estoque-backend/src/services/dashboard.service.ts
@@ -1,6 +1,7 @@
-import { isNull, sql } from "drizzle-orm";
+import { and, gte, isNull, lte, sql, sum } from "drizzle-orm";
 import { db } from "../db/connection";
-import { products } from "../db/schema";
+import { moves, products } from "../db/schema";
+import { DateRangeInput } from "../validators/dashboard.validator";
 
 export const getInventoryValue = async () => {
   const result = await db
@@ -8,7 +9,43 @@ export const getInventoryValue = async () => {
       totalValue: sql<number>`SUM(${products.quantity} * ${products.unitPrice})`
     })
     .from(products)
-    .where(isNull(products.deletedAt)) 
-  if(result[0]?.totalValue) return result[0].totalValue; 
+    .where(isNull(products.deletedAt))
+  if (result[0]?.totalValue) return result[0].totalValue;
   return 0;
+}
+
+export const getMovesSummary = async (range: DateRangeInput) => {
+  const conditions = [];
+
+  if (range.startDate) {
+    const startDate = new Date(range.startDate);
+    conditions.push(gte(moves.createdAt, startDate));
+  }
+
+  if (range.endDate) {
+    const endDate = new Date(range.endDate);
+    endDate.setUTCHours(23, 59, 59, 999);
+    conditions.push(lte(moves.createdAt, endDate));
+  }
+
+  const result = await db
+    .select({
+      type: moves.type,
+      totalValue: sql<number>`SUM(${moves.quantity} * ${moves.unitPrice})`,
+      count: sql<number>`COUNT(*)`
+    })
+    .from(moves)
+    .where(and(...conditions))
+    .groupBy(moves.type);
+
+  const sumary: Record<'in' | 'out', { value: number; count: number }> = {
+    in: { value: 0, count: 0 },
+    out: { value: 0, count: 0 }
+  }
+
+  result.forEach(row => {
+    sumary[row.type as 'in' | 'out'] = { value: row.totalValue, count: row.count }
+  });
+
+  return sumary;
 }
